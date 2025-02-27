36f1729199f284b67099d5e84a646b76ea7fd7b7
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2025-02-27 23:25:19 +0530

    Add the temporary gradle repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,11 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenLocal()
+        maven {
+            url = uri("https://znsio.github.io/specmatic-gradle-plugin")
+        }
     }
 }
+
 rootProject.name = "specmatic-gradle-plugin"
 include("plugin")
