207520d541d8246dcfa87b2c5678b561b8ec8746
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2025-02-27 23:42:07 +0530

    Code cleanup

--- plugin/src/main/kotlin/io/specmatic/gradle/ConfigureTaskInfo.kt
+++ plugin/src/main/kotlin/io/specmatic/gradle/ConfigureTaskInfo.kt
@@ -3,7 +3,7 @@ package io.specmatic.gradle
 import org.barfuin.gradle.taskinfo.GradleTaskInfoPlugin
 import org.gradle.api.Project
 
-internal class ConfigureTaskInfo(private val project: Project) {
+internal class ConfigureTaskInfo(project: Project) {
     init {
         project.plugins.apply(GradleTaskInfoPlugin::class.java)
     }
