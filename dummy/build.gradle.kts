07aa5ddfdfc756b1cc0a261ada8af5bfb498ba24
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2025-03-11 23:57:26 +0530

    Use latest 0.11

--- dummy/build.gradle.kts
+++ dummy/build.gradle.kts
@@ -3,10 +3,25 @@ buildscript {
     repositories {
         mavenCentral()
         gradlePluginPortal()
+        maven {
+            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
+            mavenContent {
+                snapshotsOnly()
+            }
+
+            content {
+                includeGroup("io.specmatic.gradle")
+            }
+        }
         mavenLocal()
     }
+
     dependencies {
-        classpath("io.specmatic.gradle:plugin:+") {
+        classpath("io.specmatic.gradle:plugin:${project.version}") {
+            isChanging = true
+        }
+
+        classpath("io.specmatic.gradle:io.specmatic.gradle.gradle.plugin:${project.version}") {
             isChanging = true
         }
     }
@@ -14,4 +29,31 @@ buildscript {
     configurations.all {
         resolutionStrategy.cacheChangingModulesFor(0, "seconds")
     }
+}
+
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    mavenLocal()
+
+    maven {
+        url = uri("https://central.sonatype.com/repository/maven-snapshots/")
+        mavenContent {
+            snapshotsOnly()
+        }
+
+        content {
+            includeGroup("io.specmatic.gradle")
+        }
+    }
+
+}
+
+dependencies {
+    implementation("io.specmatic.gradle:plugin:${project.version}")
+    implementation("io.specmatic.gradle:io.specmatic.gradle.gradle.plugin:${project.version}")
 }
