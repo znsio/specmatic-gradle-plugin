103722dc8519c689694bcc222e9135861c10201e
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2025-03-12 12:36:05 +0530

    Specify our internal repo as well

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,6 +25,21 @@ pluginManagement {
             }
         }
 
+        maven {
+            name = "specmaticPrivate"
+            url = uri("https://maven.pkg.github.com/znsio/specmatic-private-maven-repo")
+            credentials {
+                username =
+                    settings.extra["github.actor"]?.toString()
+                        ?: System.getenv("SPECMATIC_GITHUB_USER")?.toString()
+                                ?: System.getenv("ORG_GRADLE_PROJECT_specmaticPrivateUsername")?.toString()
+                password =
+                    settings.extra["github.token"]?.toString()
+                        ?: System.getenv("SPECMATIC_GITHUB_TOKEN")?.toString()
+                                ?: System.getenv("ORG_GRADLE_PROJECT_specmaticPrivatePassword")?.toString()
+            }
+        }
+
     }
 }
 
