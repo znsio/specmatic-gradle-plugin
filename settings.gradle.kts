341ee7a9c379de3b59e4102365593580d3cf127a
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2025-03-12 12:41:51 +0530

    Fix build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,11 +30,11 @@ pluginManagement {
             url = uri("https://maven.pkg.github.com/znsio/specmatic-private-maven-repo")
             credentials {
                 username =
-                    settings.extra["github.actor"]?.toString()
+                    settings.extra.properties["github.actor"]?.toString()
                         ?: System.getenv("SPECMATIC_GITHUB_USER")?.toString()
                                 ?: System.getenv("ORG_GRADLE_PROJECT_specmaticPrivateUsername")?.toString()
                 password =
-                    settings.extra["github.token"]?.toString()
+                    settings.extra.properties["github.token"]?.toString()
                         ?: System.getenv("SPECMATIC_GITHUB_TOKEN")?.toString()
                                 ?: System.getenv("ORG_GRADLE_PROJECT_specmaticPrivatePassword")?.toString()
             }
