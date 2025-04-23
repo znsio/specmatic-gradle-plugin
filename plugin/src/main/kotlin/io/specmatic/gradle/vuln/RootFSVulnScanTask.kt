f11ee53082dfebb375d253e69c0c993f87e64bc2
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2025-04-23 18:21:32 +0530

    Rejig the vuln scan tasks
    * `vulnScanJar` to scan inside jars
    * `vulnScanSBOM` to scan just match dependencies against the vuln database
    * `vulnScanDocker` to scan a docker image
    
    Additionally, ensure that scanning and license checks only works on `check`

---
+++ plugin/src/main/kotlin/io/specmatic/gradle/vuln/RootFSVulnScanTask.kt
@@ -0,0 +1,45 @@
+package io.specmatic.gradle.vuln
+
+import org.gradle.api.Project
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.process.ExecOperations
+import java.io.File
+import javax.inject.Inject
+
+abstract class RootFSVulnScanTask @Inject constructor(execLauncher: ExecOperations) :
+    AbstractVulnScanTask(execLauncher) {
+    @get:InputDirectory
+    abstract val inputDir: Property<File>
+
+    override fun getCommandLine(format: String): List<String> {
+        return listOf(trivyExecutable(), "rootfs", "--format", format, *commonArgs, inputDir.get().path)
+    }
+}
+
+internal fun Project.createJarVulnScanTask() {
+    val scanTaskName = "vulnScanJar"
+
+    val scanTask = tasks.register("${scanTaskName}Scan", RootFSVulnScanTask::class.java) {
+        dependsOn("assemble")
+
+        trivyHomeDir.set(trivyHomeDir())
+        inputDir.set(project.layout.buildDirectory.file("libs").get().asFile)
+        reportsDir.set(project.layout.buildDirectory.get().asFile.resolve("reports/vulnerabilities/jars"))
+    }
+
+    val reportTask = project.tasks.register(scanTaskName) {
+        dependsOn(scanTask)
+        group = "vulnerability"
+        description = "Print vulnerabilities in jars"
+
+        doFirst {
+            val reportFile = scanTask.get().getTextTableReportFile()
+            printReportFile(this@createJarVulnScanTask, reportFile)
+        }
+    }
+
+    project.tasks.named("check") {
+        dependsOn(reportTask)
+    }
+}
--- plugin/src/main/kotlin/io/specmatic/gradle/vuln/VulnScanTask.kt
+++ plugin/src/main/kotlin/io/specmatic/gradle/vuln/VulnScanTask.kt
@@ -1,32 +1,20 @@
 package io.specmatic.gradle.vuln
 
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.specmatic.gradle.exec.shellEscapedArgs
 import io.specmatic.gradle.license.pluginInfo
-import io.specmatic.gradle.vuln.dto.VulnerabilityReport
 import org.apache.commons.io.FileUtils
 import org.apache.commons.lang3.SystemUtils
-import org.cyclonedx.gradle.CycloneDxTask
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.*
-import org.gradle.internal.logging.text.StyledTextOutput
-import org.gradle.internal.logging.text.StyledTextOutputFactory
-import org.gradle.kotlin.dsl.support.serviceOf
 import org.gradle.process.ExecOperations
 import org.kohsuke.github.GitHubBuilder
 import java.io.File
 import java.io.FileOutputStream
 import java.io.RandomAccessFile
 import java.net.URL
-import java.text.SimpleDateFormat
-import java.time.Instant
-import java.time.temporal.ChronoUnit
-import java.util.*
 import javax.inject.Inject
 
 @CacheableTask
@@ -167,265 +155,9 @@ abstract class AbstractVulnScanTask @Inject constructor(private val execLauncher
         )
 }
 
-abstract class SBOMVulnScanTask @Inject constructor(execLauncher: ExecOperations) : AbstractVulnScanTask(execLauncher) {
+internal fun trivyHomeDir(): File = SystemUtils.getUserHome().resolve(".specmatic-trivy")
 
-    override fun getCommandLine(format: String): List<String> {
-        return listOf(
-            trivyExecutable(), "sbom", "--format", format, *commonArgs, sbomFile.get().path
-        )
-    }
-
-    @get:InputFile
-    @get:PathSensitive(PathSensitivity.RELATIVE)
-    abstract val sbomFile: Property<File>
-}
-
-abstract class ImageVulnScanTask @Inject constructor(execLauncher: ExecOperations) :
-    AbstractVulnScanTask(execLauncher) {
-    @get:Input
-    var image: String? = null
-
-    override fun getCommandLine(format: String): List<String> {
-        if (image == null) {
-            throw GradleException("image property not set")
-        }
-        return listOf(trivyExecutable(), "image", "--format", format, *commonArgs, image!!)
-    }
-}
-
-internal fun Project.createJarVulnScanTask(): TaskProvider<SBOMVulnScanTask> {
-    val scanTaskName = "vulnScanJar"
-
-    val thisProject = this
-    val printTask = project.tasks.register("${scanTaskName}Print") {
-        group = "vulnerability"
-        description = "Print vulnerabilities in $scanTaskName"
-
-        doFirst {
-            printReportFile(
-                thisProject,
-                thisProject.tasks.named(scanTaskName, SBOMVulnScanTask::class.java).get().getTextTableReportFile()
-            )
-        }
-    }
-
-    return tasks.register(scanTaskName, SBOMVulnScanTask::class.java) {
-        dependsOn(tasks.withType(CycloneDxTask::class.java))
-        finalizedBy(printTask)
-        group = "vulnerability"
-        description = "Scan for vulnerabilities in jars"
-
-        trivyHomeDir.set(trivyHomeDir())
-        sbomFile.set(project.layout.buildDirectory.get().asFile.resolve("reports/cyclonedx/bom.json"))
-
-        reportsDir.set(project.layout.buildDirectory.get().asFile.resolve("reports/vulnerabilities/source"))
-    }
-}
-
-private fun trivyHomeDir(): File = SystemUtils.getUserHome().resolve(".specmatic-trivy")
-
-internal fun Project.createDockerVulnScanTask(imageName: String): TaskProvider<ImageVulnScanTask> {
-    val scanTaskName = "vulnScanDocker"
-
-    val rootProject = this.rootProject
-
-    val printTask = rootProject.tasks.register("${scanTaskName}Print") {
-        group = "vulnerability"
-        description = "Print vulnerabilities in $scanTaskName"
-
-        doFirst {
-            val reportFile =
-                rootProject.tasks.named(scanTaskName, ImageVulnScanTask::class.java).get().getTextTableReportFile()
-            printReportFile(rootProject, reportFile)
-        }
-    }
-
-    return rootProject.tasks.register(scanTaskName, ImageVulnScanTask::class.java) {
-        dependsOn(rootProject.tasks.withType(CycloneDxTask::class.java))
-        finalizedBy(printTask)
-        group = "vulnerability"
-        description = "Scan for vulnerabilities in jars"
-        image = imageName
-
-        trivyHomeDir.set(trivyHomeDir())
-        reportsDir.set(project.layout.buildDirectory.get().asFile.resolve("reports/vulnerabilities/image"))
-    }
-}
-
-data class TableRow(
-    val type: String,
-    val packageName: String,
-    val maxSeverity: String,
-    val version: String,
-    val vulnerabilityURL: String,
-    val modified: Date,
-    val published: Date,
-    val summary: String?,
-) {
-    val formattedModified: String = formatDate(modified)
-    val formattedSeverity: String = when (maxSeverity.lowercase()) {
-        "unknown" -> "(unknown)"
-        "low" -> "$maxSeverity 🟢"
-        "medium" -> "$maxSeverity 🟡"
-        "high" -> "$maxSeverity 🟠"
-        "critical" -> "$maxSeverity 🔴"
-        else -> "(invalid severity)"
-
-    }
-    val formattedPublished: String = formatDate(published)
-    val formattedSummary: String = summary?.replace("\n", " ") ?: ""
-    val formattedPackageName: String = packageName
-    val formattedVersion: String = version
-
-    private fun formatDate(date: Date): String {
-        val daysAgo = ChronoUnit.DAYS.between(date.toInstant(), Instant.now())
-        return "${SimpleDateFormat("yyyy-MM-dd").format(date)} ($daysAgo days ago)"
-    }
-
-    fun dateColor(date: Date): StyledTextOutput.Style {
-        val modifiedColor = when (ChronoUnit.DAYS.between(date.toInstant(), Instant.now())) {
-            in 0..14 -> StyledTextOutput.Style.UserInput // Whilte
-            in 15..30 -> StyledTextOutput.Style.ProgressStatus // Amber
-            else -> StyledTextOutput.Style.Failure // Red
-        }
-        return modifiedColor
-    }
-
-    val severityColor: StyledTextOutput.Style = when (maxSeverity.lowercase()) {
-        "unknown", "low" -> StyledTextOutput.Style.UserInput // White
-        "medium" -> StyledTextOutput.Style.ProgressStatus // Amber
-        "high", "critical" -> StyledTextOutput.Style.Failure // Red
-        else -> StyledTextOutput.Style.Failure // Red
-    }
-}
-
-private fun printReportFile(project: Project, reportFile: File) {
+internal fun printReportFile(project: Project, reportFile: File) {
     project.logger.warn(reportFile.readText())
     project.logger.warn("Vulnerability report file: ${reportFile.toURI()}")
-}
-
-private fun printReportFileJSON(project: Project, reportFile: File) {
-
-    val mapper = jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        .configure(JsonParser.Feature.INCLUDE_SOURCE_IN_LOCATION, true)
-
-    val report: VulnerabilityReport = mapper.readValue(
-        reportFile, VulnerabilityReport::class.java
-    )
-
-    val tableRows = report.results.flatMap { result ->
-        result.vulnerabilities.map { eachVulnerability ->
-            TableRow(
-                type = "${result.target} (${result.type})",
-                packageName = eachVulnerability.pkgName,
-                maxSeverity = eachVulnerability.severity,
-                version = eachVulnerability.installedVersion,
-                vulnerabilityURL = eachVulnerability.primaryUrl,
-                modified = Date.from(Instant.parse(eachVulnerability.lastModifiedDate)),
-                published = Date.from(Instant.parse(eachVulnerability.publishedDate)),
-                summary = eachVulnerability.title
-            )
-        }
-    }
-
-
-    val columnWidths = mutableMapOf(
-        "Package" to "Package".length,
-        "Max Severity" to "Max Severity".length,
-        "Version" to "Version".length,
-        "Fix Version" to "Fix Version".length,
-        "Vulnerability" to "Vulnerability".length,
-        "Modified" to "Modified".length,
-        "Published" to "Published".length,
-        "Summary" to "Summary".length
-    )
-
-    tableRows.forEach { row ->
-        columnWidths["Package"] = maxOf(columnWidths["Package"]!!, row.formattedPackageName.length)
-        columnWidths["Max Severity"] = maxOf(columnWidths["Max Severity"]!!, row.formattedSeverity.length)
-        columnWidths["Version"] = maxOf(columnWidths["Version"]!!, row.formattedVersion.length)
-        columnWidths["Vulnerability"] = maxOf(columnWidths["Vulnerability"]!!, row.vulnerabilityURL.length)
-        columnWidths["Modified"] = maxOf(columnWidths["Modified"]!!, row.formattedModified.length)
-        columnWidths["Published"] = maxOf(columnWidths["Published"]!!, row.formattedPublished.length)
-        columnWidths["Summary"] = maxOf(columnWidths["Summary"]!!, row.formattedSummary.length)
-    }
-
-    val output: StyledTextOutput = project.serviceOf<StyledTextOutputFactory>().create("vulnScan")
-
-    output.style(StyledTextOutput.Style.Header).text(
-        String.format(
-            "╔%s╦%s╦%s╦%s╦%s╦%s╦%s╗\n",
-            "═".repeat(2 + columnWidths["Package"]!!),
-            "═".repeat(2 + columnWidths["Max Severity"]!!),
-            "═".repeat(2 + columnWidths["Version"]!!),
-            "═".repeat(2 + columnWidths["Vulnerability"]!!),
-            "═".repeat(2 + columnWidths["Modified"]!!),
-            "═".repeat(2 + columnWidths["Published"]!!),
-            "═".repeat(2 + columnWidths["Summary"]!!)
-        )
-    )
-
-    output.style(StyledTextOutput.Style.Header).text(
-        String.format(
-            "║ %-${columnWidths["Package"]}s ║ %-${columnWidths["Max Severity"]}s ║ %-${columnWidths["Version"]}s ║ %-${columnWidths["Vulnerability"]}s ║ %-${columnWidths["Modified"]}s ║ %-${columnWidths["Published"]}s ║ %-${columnWidths["Summary"]}s ║\n",
-            "Package",
-            "Max Severity",
-            "Version",
-            "Vulnerability",
-            "Modified",
-            "Published",
-            "Summary"
-        )
-    )
-
-    output.style(StyledTextOutput.Style.Header).text(
-        String.format(
-            "╠%s╬%s╬%s╬%s╬%s╬%s╬%s╣\n",
-            "═".repeat(2 + columnWidths["Package"]!!),
-            "═".repeat(2 + columnWidths["Max Severity"]!!),
-            "═".repeat(2 + columnWidths["Version"]!!),
-            "═".repeat(2 + columnWidths["Vulnerability"]!!),
-            "═".repeat(2 + columnWidths["Modified"]!!),
-            "═".repeat(2 + columnWidths["Published"]!!),
-            "═".repeat(2 + columnWidths["Summary"]!!)
-        )
-    )
-
-    tableRows.forEach { row ->
-        output.style(StyledTextOutput.Style.Normal).text("║ ")
-        output.style(StyledTextOutput.Style.Normal).text(row.formattedPackageName.padEnd(columnWidths["Package"]!!))
-
-        output.style(StyledTextOutput.Style.Normal).text(" ║ ")
-        output.style(row.severityColor).text(row.formattedSeverity.padEnd(columnWidths["Max Severity"]!!))
-
-        output.style(StyledTextOutput.Style.Normal).text(" ║ ")
-        output.style(StyledTextOutput.Style.Normal).text(row.formattedVersion.padEnd(columnWidths["Version"]!!))
-
-        output.style(StyledTextOutput.Style.Normal).text(" ║ ")
-        output.style(StyledTextOutput.Style.Normal).text(row.vulnerabilityURL.padEnd(columnWidths["Vulnerability"]!!))
-
-        output.style(StyledTextOutput.Style.Normal).text(" ║ ")
-        output.style(row.dateColor(row.modified)).text(row.formattedModified.padEnd(columnWidths["Modified"]!!))
-
-        output.style(StyledTextOutput.Style.Normal).text(" ║ ")
-        output.style(row.dateColor(row.published)).text(row.formattedPublished.padEnd(columnWidths["Published"]!!))
-
-        output.style(StyledTextOutput.Style.Normal).text(" ║ ")
-        output.style(StyledTextOutput.Style.Normal).text(row.formattedSummary.padEnd(columnWidths["Summary"]!!))
-
-        output.style(StyledTextOutput.Style.Normal).text(" ║\n")
-    }
-
-    output.style(StyledTextOutput.Style.Header).text(
-        String.format(
-            "╚%s╩%s╩%s╩%s╩%s╩%s╩%s╝\n",
-            "═".repeat(2 + columnWidths["Package"]!!),
-            "═".repeat(2 + columnWidths["Max Severity"]!!),
-            "═".repeat(2 + columnWidths["Version"]!!),
-            "═".repeat(2 + columnWidths["Vulnerability"]!!),
-            "═".repeat(2 + columnWidths["Modified"]!!),
-            "═".repeat(2 + columnWidths["Published"]!!),
-            "═".repeat(2 + columnWidths["Summary"]!!)
-        )
-    )
 }
